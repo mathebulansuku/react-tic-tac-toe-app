9641a6274dd8dc86ed965875878f00ee3d500bee
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-06 00:52:12 +0200

    Player name added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import Header from "../src/components/Header";
+import Player from "../src/components/Player";
 
 function App() {
   return (
-    <div>
+    <menu>
       <Header title="Tic-Tac-Toe" />
-    </div>
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player 1" symbol="X" />
+          <Player name="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+      LOG
+    </menu>
   );
 }
 
