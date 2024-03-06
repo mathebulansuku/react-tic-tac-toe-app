ff4965a7f27ab05a54da48d317b988b327865678
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-06 04:02:37 +0200

    gameboard update

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
+import { useState } from "react";
 import Header from "../src/components/Header";
 import Player from "../src/components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare() {
+    setActivePlayer((currentActivePlayer) =>
+      currentActivePlayer === "X" ? "O" : "X"
+    );
+  }
+
   return (
     <menu>
       <Header title="Tic-Tac-Toe" />
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handleSelectSquare}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOG
     </menu>
--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -5,7 +5,7 @@ const initGameBoard = [
   [null, null, null],
   [null, null, null],
 ];
-export default function GameBoard() {
+export default function GameBoard({ onSelectSquare, activePlayerSymbol }) {
   const [gameBoard, setGameBoard] = useState(initGameBoard);
 
   function handleClick(rowIndex, colIndex) {
@@ -13,9 +13,11 @@ export default function GameBoard() {
       const updatedBoard = [
         ...prevGameBoard.map((innerArray) => [...innerArray]),
       ];
-      updatedBoard[rowIndex][colIndex] = "X";
+      updatedBoard[rowIndex][colIndex] = activePlayerSymbol;
       return updatedBoard;
     });
+
+    onSelectSquare();
   }
   return (
     <ol id="game-board">
