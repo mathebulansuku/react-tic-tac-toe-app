bcfa1a0435cba56fdb3e447948b5984d23fef1fe
Nsuku Mathebula <mathebulansuku@gmail.com>
2024-03-06 03:13:38 +0200

    Player component complete

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,18 +1,29 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
   const [isEditing, setIsEditing] = useState(false);
 
   function handleClick() {
     setIsEditing((isEditing) => !isEditing);
   }
 
+  function handleChange(event) {
+    let changeName = event.target.value;
+    setPlayerName(changeName);
+  }
+
   return (
     <li>
       {!isEditing ? (
-        <span className="player-name">{name}</span>
+        <span className="player-name">{playerName}</span>
       ) : (
-        <input type="text" required value={name} />
+        <input
+          type="text"
+          required
+          value={playerName}
+          onChange={handleChange}
+        />
       )}
       <span className="player">
         <span className="player-symbol">{symbol}</span>
